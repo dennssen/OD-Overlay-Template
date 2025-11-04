a8d0caa54a160a19c6853845d4add43d9b046211
dennssen <dennssen@gmail.com>
2025-11-04 13:23:57 +0100

    minimap start and svgs

---
+++ Webpage/Overlays/DefaultTemplate/Minimap/minimap.js
@@ -0,0 +1,40 @@
+async function updateOverlay() {
+    const camera_id = "dennssen.overlayInfo";
+
+    try {
+        const cameraConfigResponse = await fetch(`http://localhost:5420/cameras/${camera_id}/config`);
+
+        if (!cameraConfigResponse.ok) {
+            return
+        }
+
+        const cameraConfig = await cameraConfigResponse.json();
+
+        const extraArenaInfo = cameraConfig.extraArenaInfo;
+        const slot_id = cameraConfig.gamemodeSlotId;
+
+        const gamemodeResponse = await fetch(`http://localhost:5420/state/gamemodes/${slot_id}`)
+
+        if (!gamemodeResponse.ok) {
+            return
+        }
+
+        const gamemode = await gamemodeResponse.json()
+
+        setTeamColors(gamemode)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+setInterval(updateOverlay, 250)
+
+function setTeamColors(gamemode) {
+    const root = document.querySelector(":root")
+
+    const homeColor = gamemode.teams[0].teamColor.primary
+    const awayColor = gamemode.teams[1].teamColor.primary
+
+    root.style.setProperty("--homeColor", `rgba(${homeColor.r}, ${homeColor.g}, ${homeColor.b}, ${homeColor.a})`)
+    root.style.setProperty("--awayColor", `rgba(${awayColor.r}, ${awayColor.g}, ${awayColor.b}, ${awayColor.a})`)
+}
